77c2763029836aeda91f70520b424eb57a2dcfc5
ozaki <fs65ugumori@yahoo.co.jp>
2019-08-05 22:34:23 +0900

    add routing

---
+++ src/constants/routes.js
@@ -0,0 +1,9 @@
+const Signin = '/users/signin';
+const Signup = '/users/signup';
+const Menu = '/';
+
+export default {
+  Signin,
+  Signup,
+  Menu,
+};
---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import ROUTES from 'constants/routes';
+import ScrollToTop from 'router/ScrollToTop';
+import Signin from 'containers/Signin';
+import Signup from 'containers/Signup';
+import Menu from 'containers/Menu';
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <>
+          <ScrollToTop />
+          <Route path={ROUTES.Signin} exact component={Signin} />
+          <Route path={ROUTES.Signup} exact component={Signup} />
+          <Route path={ROUTES.Menu} exact component={Menu} />
+        </>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
