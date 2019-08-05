67d205e68533a3cc190cc0f892d72b08e0205986
ozaki <fs65ugumori@yahoo.co.jp>
2019-08-06 08:07:32 +0900

    認証周り既存実装の移植

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import Router from 'router';
+import Firebase from 'utils/firebase';
+import FirebaseContext from 'context/firebase';
+import { withAuthentication } from 'hoc/sessions';
+
+const AuthRouter = withAuthentication(Router);
 
 function App() {
-  return <Router />;
+  return (
+    <FirebaseContext.Provider value={new Firebase()}>
+      <AuthRouter />
+    </FirebaseContext.Provider>
+  );
 }
 
 export default App;
--- src/containers/Menu.js
+++ src/containers/Menu.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Menu from 'components/pages/Menu';
 import useReactRouter from 'use-react-router';
+import { withAuthorization } from 'hoc/sessions';
 
 function MenuContainer() {
   const { history } = useReactRouter();
   return <Menu history={history} />;
 }
 
-export default MenuContainer;
+export default withAuthorization(MenuContainer);
