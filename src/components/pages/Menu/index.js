ded759408673e104383c83411f238abdda0244a0
ozaki <fs65ugumori@yahoo.co.jp>
2019-08-22 08:14:27 +0900

    update Menu

--- src/components/pages/Menu/index.js
+++ src/components/pages/Menu/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Container from 'components/templates/Container';
 
-function Menu({ history }) {
+function Menu({ history, authUser }) {
+  const { uid, displayName } = authUser || {};
   return (
     <Container title="メニュー" history={history}>
       <h1>Menu</h1>
+      <p>
+        {uid}: {displayName}
+      </p>
     </Container>
   );
 }
