c57700817275e4d17a2a2d8e31749c8fd061fb77
ozaki <fs65ugumori@yahoo.co.jp>
2019-08-22 23:08:29 +0900

    add padding to Container

--- src/components/templates/Container/index.js
+++ src/components/templates/Container/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import NavigationBar from 'components/organisms/NavigationBar';
 
-function Container({ children, title, back, noHeader, history }) {
+function Container({ children, title, back, noHeader, noPadding, history }) {
   return (
     <>
       {noHeader || (
         <NavigationBar title={title} history={history} back={back} />
       )}
-      <div>{children}</div>
+      <div style={{ padding: noPadding ? 'inherit' : '5px 10px' }}>
+        {children}
+      </div>
     </>
   );
 }
