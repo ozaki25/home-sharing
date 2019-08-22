88bf1ab66d11b41e6651f55331872bc286ac6b79
ozaki <fs65ugumori@yahoo.co.jp>
2019-08-22 23:42:30 +0900

    update Container

--- src/components/templates/Container/index.js
+++ src/components/templates/Container/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import NavigationBar from 'components/organisms/NavigationBar';
 
-function Container({ children, title, back, noHeader, noPadding, history }) {
+function Container({
+  children,
+  title,
+  back,
+  noHeader,
+  noPadding,
+  authUser,
+  history,
+  firebase,
+}) {
   return (
     <>
       {noHeader || (
-        <NavigationBar title={title} history={history} back={back} />
+        <NavigationBar
+          title={title}
+          authUser={authUser}
+          history={history}
+          firebase={firebase}
+          back={back}
+        />
       )}
       <div style={{ padding: noPadding ? 'inherit' : '5px 10px' }}>
         {children}
