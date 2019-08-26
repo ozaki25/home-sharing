1ce448d63be751035da5679f9d9d84d7b1869d77
ozaki <fs65ugumori@yahoo.co.jp>
2019-08-27 08:25:06 +0900

    update menu

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,11 +1,15 @@
 const Signin = '/users/signin';
 const Signup = '/users/signup';
 const Menu = '/';
+const Lend = '/houses/lend';
+const Rent = '/houses/rent';
 const AccountSetting = '/settings/account';
 
 export default {
   Signin,
   Signup,
   Menu,
+  Lend,
+  Rent,
   AccountSetting,
 };
